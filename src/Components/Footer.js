31ee57e733099529559ffb1d534f433d200d7f5f
mwangi <izzyaaron2021@gmail.com>
2024-11-04 10:01:02 +0300

    Add responsive Google Maps iframe in Location component

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,10 +7,10 @@ function Footer() {
       <div className="box-container">
         <div className="box">
           <h3>contact info</h3>
-          <a href="tel:+2547 68302868"><i className="fas fa-phone"></i> +254768302868</a>
-          <a href="tel:+254798 955 211"><i className="fas fa-phone"></i> +254798 955 211</a>
+          <a href="tel:+2547 68302868"><i className="fas fa-phone"></i> +254768-302-868</a>
+          <a href="tel:+254798 955 211"><i className="fas fa-phone"></i> +254798-955-211</a>
           <a href="mailto:limashotel@gmail.com"><i className="fas fa-envelope"></i> limashotel@gmail.com</a>
-          <a href="https://maps.google.com/?q=Kibomet,+Kitale" target="_blank" rel="noopener noreferrer"><i className="fas fa-map"></i> Kibomet, Kitale</a>
+          <a href="https://www.google.com/maps/place/Club+limas,+hotel+and+accomodation/@1.0355365,35.0251838,16z/data=!4m10!1m2!2m1!1sclub+limas+kitale!3m6!1s0x1782279d1ad61f7b:0xc538e21a7d7f8896!8m2!3d1.0352417!4d35.0309186!15sChFjbHViIGxpbWFzIGtpdGFsZZIBBGNsdWLgAQA!16s%2Fg%2F11j93wg32q?entry=ttu&g_ep=EgoyMDI0MTAyOS4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer"><i className="fas fa-map"></i> Club limas, Kitale</a>
         </div>
         <div className="box">
           <h3>quick links</h3>
--- src/Components/Location.js
+++ src/Components/Location.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import locationImage from '../Assets/images/lomas.jpg'; // Adjust the path as needed
-import '../Css/Location.css'; // Create this CSS file to style your component
+import '../Css/Location.css';
 
 const Location = () => {
   return (
     <div className="location-container">
-      <img src={locationImage} alt="Our Location" className="location-image" />
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15955.777611774992!2d35.0251838!3d1.0355365!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1782279d1ad61f7b%3A0xc538e21a7d7f8896!2sClub%20limas%2C%20hotel%20and%20accommodation!5e0!3m2!1sen!2ske!4v1665814732280!5m2!1sen!2ske"
+        width="600"
+        height="450"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        title="Club Limas Location"
+      ></iframe>
     </div>
   );
 };
